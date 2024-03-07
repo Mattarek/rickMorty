6fa2dff44b8caf396e35dd0a1c38aeb339401715
Marcin Barc <marb637@gmail.com>
2024-03-07 11:01:24 +0100

    CharacterProps changed, episode changed to array of object

--- src/components/character/Character.tsx
+++ src/components/character/Character.tsx
@@ -1,35 +1,31 @@
-import { ApiData, CharacterProps } from '../../types/Api';
+import { CharacterProps } from '../../types/Api';
 
-export const Character = ({ isPending, data }: CharacterProps) => {
+export const Character = ({ isPending, data: { results } }: CharacterProps) => {
     return (
         <div>
             {isPending ? (
                 <div>Loading...</div>
             ) : (
-                data?.results?.map(
-                    ({ name, id, image, species, episode }: ApiData) => {
-                        return (
-                            <div key={id}>
-                                <div>{name}</div>
-                                <img
-                                    src={image}
-                                    alt=''
-                                />
-                                <div>{species}</div>
-                                <ul>
-                                    Seen in episodes:{' '}
-                                    {episode.map((element) => {
-                                        return (
-                                            <li>
-                                                {element[element.length - 1]}
-                                            </li>
-                                        );
-                                    })}
-                                </ul>
-                            </div>
-                        );
-                    },
-                )
+                results?.map(({ name, id, image, species, episode }) => {
+                    return (
+                        <div key={id}>
+                            <div>{name}</div>
+                            <img
+                                src={image}
+                                alt=''
+                            />
+                            <div>{species}</div>
+                            <ul>
+                                Seen in episodes:{' '}
+                                {episode.map((element) => {
+                                    return (
+                                        <li>{element[element.length - 1]}</li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    );
+                })
             )}
         </div>
     );
--- src/types/Api.ts
+++ src/types/Api.ts
@@ -1,19 +1,13 @@
-export interface ApiData {
-    name: string;
-    id: number;
-    image: string;
-    species: string;
-    episode: string[];
-}
-
 export interface CharacterProps {
     isPending: boolean;
     data: {
-        name: string;
-        id: number;
-        image: string;
-        species: string;
-        episode: string;
-        results: string[];
+        results: Array<{
+            name: string;
+            id: number;
+            image: string;
+            species: string;
+            episode: string[];
+            results: Array<string>;
+        }>;
     };
 }
